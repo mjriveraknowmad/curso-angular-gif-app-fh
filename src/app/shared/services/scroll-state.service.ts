faff1088a8902b5e9e67a541595669b3bcce76fd
Marcos José Rivera Souto <mjrivera@knowmadmood.com>
2026-09-21 17:35:11 +0200

    refactor: eliminación del changeEffect usado para logear

--- src/app/shared/services/scroll-state.service.ts
+++ src/app/shared/services/scroll-state.service.ts
@@ -4,10 +4,6 @@ import { Injectable, signal, effect } from '@angular/core';
 export class ScrollStateService {
   #trendingScrollState = signal(0);
 
-  changeEffect = effect(() => {
-    console.log('Trending scroll state changed:', this.#trendingScrollState());
-  });
-
   set trendingScrollState(value: number) {
     this.#trendingScrollState.set(value);
   }
